9cb0ce260c5f696633ba1cf55b2a9647886b5858
Adam Stogsdill <adam.stogsdill@gmail.com>
2022-01-21 12:37:07 -0600

    Adding some new changes and documentation for methods

--- DatabaseIO.py
+++ DatabaseIO.py
@@ -1,15 +1,23 @@
+import os
 import pyodbc
 import json
+from azure.storage.blob import BlobServiceClient, ContainerClient, __version__
+from azure.storage.blob.aio import BlobClient
+
+__container_name__ = None
+__connection_str__ = None
 
 class DatabaseHandler:
     
-    def __init__(self):
+    def __init__(self, json_file_location: str):
         self.conn = None
-        pass
+        self.cursor = None
+        self.connect(json_file_location=json_file_location)
     
     def connect(self, json_file_location: str):
         '''
         connect to the db, open a buffered cursor
+        
         Returns
         -------
         bool
@@ -27,18 +35,16 @@ class DatabaseHandler:
         port = api_information['port']
         connstr = 'DRIVER='+driver+';SERVER='+server+';PORT='+port+';DATABASE='+database+';UID='+username+';PWD='+ password
 
-
         try:
             conn = pyodbc.connect(connstr)
-            global cursor
-            cursor=conn.cursor()
+            cursor = conn.cursor()
             return True
 
         except Error as e:
             print(e)
             return False
         
-    def finish(self):
+    def close(self):
         '''
         Closes DB connection
         
@@ -48,4 +54,22 @@ class DatabaseHandler:
         '''
         
         self.conn.close()
+        
+    def download(self, blob_url: str, download_file_location: str, download_file_name: str):
+        try:
+            blob_service_client = BlobServiceClient.from_connection_string(__connection_str__)
+            
+            blob_client = blob_service_client.get_blob_client(container=__container_name__, blob=blob_url)
+            
+            # print("\Downloading to Local Storage as blob:\n\t" + blob_storage_url)
+            
+            # Upload the file
+            file_name = os.path.join(download_file_location, download_file_name)
+            with open(file_name, "wb") as local_blob:
+                blob_data = blob_client.download_blob()
+                blob_data.readinto(local_blob)
+    
+        except Exception as ex:
+            print('Exception:')
+            print(ex)
     
--- DatasetManager.py
+++ DatasetManager.py
@@ -7,19 +7,44 @@ from DatabaseIO import DatabaseHandler
 
 class DatasetManager():
 
-    def __init__(self, folder_location: str):
+    def __init__(self, dataset_id: str, folder_location: str):
+        """
+        
+        The DatasetManager class handles the dataset information. Essentially,
+        this class will help manage the json information corresponding to 
+        a certain dataset and its id.
+
+        Args:
+            dataset_id (str): The ID of the dataset within the database download
+            folder_location (str): The folder location with the json data that you would like to load
+                This should include all of the dataset IDs.
+        """
+        self.dataset_id = dataset_id
         self.dataset_location = folder_location
         self.metadata = {"dataset_changes":{}, "manual_metadata_changes":{}}
         
     def load_from_json(self, metadata_location: str):
+        """ Load the metadata information from a given file location.
+
+        Args:
+            metadata_location (str): File location of the metadata json information.
+        """
         with open(metadata_location) as file:
-            self.metadata = json.load(file)
+            self.metadata = json.load(file)[self.dataset_id]
             
     def load_from_azure_database(self, databaseHandler: DatabaseHandler):
+        """[summary]
+
+        Args:
+            databaseHandler (DatabaseHandler): [description]
+        """
         pass
+    
+    def get_metadata(self):
+        return self.metadata
 
     def set_metadata(self, metadata: File):
-        self.metadata = json.load(metadata)
+        self.metadata = json.load(metadata)[self.dataset_id]
 
     def append_process(self, process: str):
         utc_time = datetime.utcnow()
@@ -39,4 +64,30 @@ def FileManager():
     
     def __init__(self, file_location: str):
         self.file_location = file_location
-        self.metadata = {}
+        self.metadata = {"file_changes":{}, "manual_metadata_changes":{}}
+        
+    def load_from_json(self, metadata_location: str):
+        with open(metadata_location) as file:
+            self.metadata = json.load(file)
+            
+    def load_from_azure_database(self, databaseHandler: DatabaseHandler):
+        pass
+    
+    def get_metadata(self):
+        return self.metadata
+
+    def set_metadata(self, metadata: File):
+        self.metadata = json.load(metadata)
+
+    def append_process(self, process: str):
+        utc_time = datetime.utcnow()
+        self.metadata["file_changes"][str(utc_time)] = process
+        self.metadata["manual_metadata_changes"] = ("append_process", process, str(utc_time))
+        
+    def delete_process(self, process: str):
+        if len(self.metadata.keys()) == 0:
+            raise Exception("METADATA LACKS INFORMATION")
+        for timestamp in self.metadata:
+            if self.metadata["dataset_changes"][timestamp] == process:
+                utc_time = datetime.utcnow()
+                self.metadata["manual_metadata_changes"] = ('delete_process', process, str(utc_time))
